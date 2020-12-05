fabfeed5f3c2ec8d27a4d6b881358a8e74bd824f
Elwilliko <52544334+Elwilliko@users.noreply.github.com>
2020-12-05 11:07:53 -0500

    Update ContextJDBC.java

--- src/ups/edu/ec/jdbc/ContextJDBC.java
+++ src/ups/edu/ec/jdbc/ContextJDBC.java
@@ -22,7 +22,7 @@ import java.sql.Statement;
  */
 public class ContextJDBC {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3307/mydb1";
+	private static final String URL = "jdbc:mysql://localhost:3307/mydb1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final String USER = "root";
 	private static final String PASS = "Patito.123";
 
