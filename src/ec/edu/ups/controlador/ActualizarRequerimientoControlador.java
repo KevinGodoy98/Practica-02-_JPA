681930146a9681b5c041140e96c39b6f47c98a92
Elwilliko <52544334+Elwilliko@users.noreply.github.com>
2020-12-06 16:06:19 -0500

    cambio
    datos

--- src/ec/edu/ups/controlador/ActualizarRequerimientoControlador.java
+++ src/ec/edu/ups/controlador/ActualizarRequerimientoControlador.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 import ec.edu.ups.dao.DAOFactory;
 import ec.edu.ups.dao.RequerimientosCompraDAO;
 import ups.edu.ec.modelo.RequerimientosCompra;
-
+import ups.edu.ec.modelo.*;
 /**
  * Servlet implementation class ActualizarRequerimientoControlador
  */
